379ba390865e6b3d8440a31f47b5e8c144b6b4f9
Harshada Bhangale <harshadabhangale21@gmail.com>
2023-05-18 09:54:56 +0530

    initial commit

---
+++ src/May17.java
@@ -0,0 +1,32 @@
+public class May17 {
+    public static void main(String args[])
+    {
+        int a=10;
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(~a);
+        boolean flag= false;
+        System.out.println(!flag);
+
+
+        int b=5;
+        System.out.println(b<<2);
+        System.out.println(32>>3);
+    int c=2;
+    System.out.println(a<b? "b is greater " : "a is greater");
+
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+
+
+    }
+}
+   /* git add
+    git commit
+    git push origin master
+    git branch */
